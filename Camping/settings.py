4e0a39db866afb3d9fb5989a8742c73df7d7c583
Jcan20 <brayan.cando4942@utc.edu.ec>
2025-01-10 00:21:06 -0500

    eje4

--- Camping/settings.py
+++ Camping/settings.py
@@ -63,11 +63,9 @@ WSGI_APPLICATION = 'Camping.wsgi.application'
 # Database
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
-        conn_max_age=600
+        default=os.getenv('DATABASE_URL')
     )
 }
-
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {
--- build.sh
+++ build.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
-# Exit on error
-set -o errexit
-
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+python -m pip install --upgrade pip
 pip install -r requirements.txt
-
-# Convert static asset files
-python manage.py collectstatic --noinput
-
-# Apply any outstanding database migrations
-python manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
